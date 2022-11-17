83d370c77f10696b584ced15ebfb564a22f49862
Anurag Pathak <pathakanurag605@gmail.com>
2022-11-17 14:18:08 +0530

    Time: 275 ms (79.76%), Space: 74.2 MB (30.19%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
         vector<int> res(n);
-        for (auto v : bookings) {
-            res[v[0] - 1] += v[2];
-            if (v[1] < n) {
-                res[v[1]] -= v[2];
+        for (auto book : bookings) {
+            res[book[0] - 1] += book[2];
+            if (book[1] < n) {
+                res[book[1]] -= book[2];
             }
         }
         for (int i = 1; i < n; ++i) { 
