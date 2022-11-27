b6bea7ece1e58119b4039bb5adfbf5769346c784
Anurag Pathak <pathakanurag605@gmail.com>
2022-11-27 16:05:09 +0530

    Time: 255 ms (75.74%), Space: 46.4 MB (11.52%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,47 +1,58 @@
 class Solution {
 public:
-    int findmax(vector<vector<int>> mat, int r){
-        int maxe=INT_MIN;
-        int idx=0;
-        for(int i=0;i<mat[0].size();i++){
-            
-            if(maxe<mat[r][i]){
-                maxe=mat[r][i];
-                idx=i;
+
+int find_max_in_each_row(vector<vector<int>>&mat, int r){
+        int column = 0;
+        int Max = INT_MIN;
+        
+        for(int c=0; c<mat[0].size(); c++){ //humme ek row mai max element chahiye 
+           Max = max(mat[r][c],Max);
+            if(Max == mat[r][c]){
+                column = c;
             }
         }
-        return idx;
+        return column;
     }
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
+        int n = mat.size();
+        int m = mat[0].size();
+        int column;
         int low = 0;
-        int high = m-1;
-        while(low <= high){
-            int mid=(low + (high - low) / 2);
-            int c = findmax(mat , mid);
-            int val = mat[mid][c];
-            if(mid == 0){
-                if(mat[mid+1][c] < val){
-                    return {mid , c};
+        int high = n-1;
+        
+        while(low<=high){
+            
+            int mid = low + (high-low)/2;
+            column =  find_max_in_each_row(mat, mid);
+            
+           if(mid == 0){
+                if(mat[mid][column]>mat[mid+1][column]){
+                    return{mid,column};
                 }
             }
-            else if(mid==m-1){
-                if(mat[mid-1][c]<val){
-                    return {mid , c};
+            
+            else if(mid == n-1){
+                if(mat[mid][column]>mat[mid-1][column]){
+                    return{mid,column};
                 }
             }
-            else if(val>mat[mid+1][c] && val>mat[mid-1][c]){
-                return {mid , c};
-            }
-            if( val<mat[mid+1][c]){                        
-                                    
-                low = mid + 1;
-            }
-            else{                     
-                high = mid - 1;
-            }
-        }
-        return {-1 , -1};
+            
+            else if(mat[mid][column]>mat[mid+1][column] && mat[mid][column]>mat[mid-1][column]){ //we have                got the greatest element in a row to agal bagaldeakhne ki zarorat nhi hai 
+                return {mid,column};
+                
+             }
+            if(mat[mid][column]<mat[mid+1][column])
+                low=mid+1;
+            
+            else
+                high = mid-1;
+            
+        }      
+        
+     return {-1,-1};   
+        
+        
+        
     }
 };
+
