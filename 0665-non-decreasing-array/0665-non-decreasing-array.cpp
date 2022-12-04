13ad13edaa53638be0a5aefe817cdcdee32fb671
Anurag Pathak <pathakanurag605@gmail.com>
2022-12-05 01:59:09 +0530

    Time: 36 ms (81.08%), Space: 26.9 MB (83.37%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.cpp
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -4,8 +4,7 @@ public:
         int count = 0;
         for(int i = 0; i < nums.size() - 1; i++){
             if(nums[i] > nums[i+1]){
-                count++;
-                if(count > 1){
+                if(++count > 1){
                     return false;
                 }
                 if(i + 1 == 1 || nums[i-1] <= nums[i+1]){
